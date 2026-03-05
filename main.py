2725f7fb6102445eb327cbb990302183fa5a35e6
lynx-hrisi <hrishikeshrajbhar118@gmail.com>
2026-03-05 20:44:44 +0530

    Added index.html

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from getSensorDataService import makeConnectionToComPort, getSensorsDataService, returnSensorDataService
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+# app.mount("/static")
+templates = Jinja2Templates(directory="templates")
 
 serialInstance = makeConnectionToComPort()
 
 @app.get("/")
-async def home():
-    return { "msg": "Server is running" }
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/get-data")
 async def get_sensor_data():
